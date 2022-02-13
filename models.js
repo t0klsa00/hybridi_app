0fd585c0bd03bee7f3352f268e04c1b35f7d5cde
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2022-02-13 14:09:46 +0200

    asd

--- models.js
+++ models.js
@@ -18,10 +18,9 @@ const getRestaurants = () => {
     }) 
   }
 
-  const getMenuById = (body) => {
+  const getMenuById = (conn_id) => {
     return new Promise(function(resolve, reject) {
-      body = JSON.parse(JSON.stringify(body[0]))
-      const { id } = body
+      const { id } = conn_id
       client.query("SELECT * FROM menus WHERE conn_id = $1", [id], (error, results) => {
         if (error) {
           reject(error)
